a35fd5cc4f7bc6e6bb24e8ed7c3ea73c5eb33aef
Edgaras <Str4iker@gmail.com>
2023-08-08 01:03:41 +0300

    end of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection service,
+            IConfiguration config)
+        {
+            service.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            service.AddCors();
+            service.AddScoped<ITokenService, TokenService>();
+
+            return service;
+        }
+    }
+}
