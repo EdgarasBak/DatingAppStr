36a5d72aa40371550aa1dff3c9bdd529c2246913
Edgaras <Str4iker@gmail.com>
2023-08-21 22:44:46 +0300

    end of saction 7

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -10,7 +10,7 @@ import { User } from '../_models/user';
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
   private currentUserSource = new BehaviorSubject<User | null>(null);
-  currentUser$ = this.currentUser$.asObservable();
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
